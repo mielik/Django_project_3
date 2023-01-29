ec701565c5b9d18a9f6a3ff7c7f1b9fa0b683264
Derbasova <anna1.derbasova@gmail.com>
2023-01-29 12:21:45 +0100

    PEP8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,16 +5,22 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, verbose_name='Title of the group', help_text='Дайте название группе до 200 символов')
-    slug = models.SlugField(unique=True, verbose_name='URL label', help_text='Название URL')
-    description = models.TextField(verbose_name='Description', help_text='Опишите группу')
+    title = models.CharField(max_length=200,
+                             verbose_name='Title of the group',
+                             help_text='Дайте название группе до 200 символов')
+    slug = models.SlugField(unique=True,
+                            verbose_name='URL label',
+                            help_text='Название URL')
+    description = models.TextField(verbose_name='Description',
+                                   help_text='Опишите группу')
 
     def __str__(self) -> str:
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField(verbose_name='Text', help_text='Напишите текст для своего поста')
+    text = models.TextField(verbose_name='Text',
+                            help_text='Напишите текст для своего поста')
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name='Publication date',
